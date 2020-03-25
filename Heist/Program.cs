af4666cb4886fcf64fe6183bf3b96278506da0d6
EvanG <evangdesigns@gmail.com>
2020-03-24 20:53:32 -0500

    Blah...

--- Heist/Program.cs
+++ Heist/Program.cs
@@ -7,6 +7,26 @@ namespace Heist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist");
+            Console.WriteLine("What is Your Team Member's Name");
+            var MemberName = Console.ReadLine();
+
+            Console.WriteLine("What is Your Team Member's Skill Level");
+            var SkillLevel = Console.ReadLine();
+
+            string CourageFactor;
+            bool IsCourageFactorOutOfRange;
+            do
+            {
+                Console.WriteLine("What is Your Team Member's Courage Factor (Please enter an decimal between 0.0 and 2.0)");
+                CourageFactor = Console.ReadLine();
+                IsCourageFactorOutOfRange = decimal.Parse(CourageFactor) < 0.0m || decimal.Parse(CourageFactor) > 2.0m;
+            }
+            while (IsCourageFactorOutOfRange);
+            
+
+            var TeamMember = new TeamMember(MemberName, SkillLevel, CourageFactor);
+
+            TeamMember.AnnounceMember();
         }
     }
 }
