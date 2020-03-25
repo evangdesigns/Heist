be726cdf32c4992d835d1a597c1dd2cd4a6c9996
EvanG <evangdesigns@gmail.com>
2020-03-24 19:03:42 -0500

    Add project files.

---
+++ Heist/TeamMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heist
+{
+    class TeamMember
+    {
+        public string Name { get; set; }
+        int SkillLevel { get; set; }
+        double CourageFactor { get; set; }
+    }
+}
